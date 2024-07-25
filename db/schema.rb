5ba65ea3d527ff389f3bb60ffb761b10268a9407
Dimitri Almon <dimitri.almon@gmail.com>
2024-07-25 19:34:33 +0200

    Updates

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_21_152647) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "username", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
